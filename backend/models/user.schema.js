aea93a13657fa5ab28e1ad1967f7ba4e673a6080
HarshKunte <harshkunte99@gmail.com>
2023-03-15 10:31:58 +0530

    User schema added

---
+++ backend/models/user.schema.js
@@ -0,0 +1,86 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import JWT from 'jsonwebtoken';
+import crypto from 'crypto';
+import config from '../config/index.js'
+
+const userSchema = mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: [true, "Name is required"],
+            maxLength: [50, "Name must be less than 50 chars"],
+            trim: true
+        },
+        email:{
+            type: String,
+            required: [true, "Email is required"],
+            unique:true,
+        },
+        password:{
+            type: String,
+            required: [true, "Password is required"],
+            minLength: [8, "Password must be atleast 8 chars"],
+            select: false // This makes sure whenever query data is returned password is not returned with it.
+        },
+        mobile:{
+            type:String,
+            required :[true, "Mobile number is required"],
+            maxLength:[10,"Mobile number is invalid"],
+        },
+        company_name:{
+            type:String,
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date
+    },
+    {
+        timestamps: true
+    }
+)
+
+//pre method to encrypt password
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) 
+    return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+    next()
+})
+
+
+// schema methods
+userSchema.methods = {
+    //compare password
+    comparePassword : async function(enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password);
+    },
+    
+    //generate JWT token
+    getJwtToken: function () {
+        return JWT.sign(
+            {
+                _id: this._id,
+                role: this.role
+            },
+            config.JWT_SECRET,
+            {
+                expiresIn: config.JWT_EXPIRY
+            }
+        )
+        
+    },
+
+    generateForgotPasswordToken: function(){
+        const forgotToken = crypto.randomBytes(20).toString('hex')
+
+        //encrypt and save to DB
+        this.forgotPasswordToken = crypto.createHash("sha256").update(forgotToken).digest('hex')
+
+        this.forgotPasswordExpiry = Date.now()+20*60*1000
+
+        return forgotToken
+    }
+}
+
+export default mongoose.model('User', userSchema)
