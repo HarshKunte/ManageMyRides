1b84a8cc549cf8812884d3bab524a82eee6f09a8
HarshKunte <harshkunte99@gmail.com>
2023-03-15 22:36:03 +0530

    forgot and reset password functionality completed

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router()
 
 //controller imports
-import {changePassword, forgotPassword, login, logout, signUp} from '../controllers/auth.controller.js'
+import {forgotPassword, login, logout, resetPassword, signUp} from '../controllers/auth.controller.js'
 
 //user signup
 router.post('/auth/signup', signUp);
@@ -15,5 +15,8 @@ router.post('/auth/logout', logout);
 
 //change password
 router.post('/auth/password/forgot', forgotPassword);
+
+//change password
+router.post('/auth/password/reset/:token', resetPassword);
 
 export default router
