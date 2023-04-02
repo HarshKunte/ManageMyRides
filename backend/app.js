635f203b340250bd9f09014099b9d893c46bdccd
HarshKunte <harshkunte99@gmail.com>
2023-04-03 01:50:15 +0530

    changes to avoid cors error

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.options('/*', (_, res) => {
 app.use(cookieParser())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     if(req.method === 'OPTIONS') {
