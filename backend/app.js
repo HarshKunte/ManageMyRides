dcb808921ffacea3f30718edd5e11e52b05e3b34
HarshKunte <harshkunte99@gmail.com>
2023-04-03 01:43:01 +0530

    changes for deployment

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,12 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
     next()
   });
 
